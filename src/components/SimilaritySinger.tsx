3d5c906d0c3e56dd8febdadc18839376b0fad0a4
TKparadise <paradiseoftk@gmail.com>
2020-09-24 17:02:24 +0900

    apiを頑張って取得しようのコーナー

--- src/components/SimilaritySinger.tsx
+++ src/components/SimilaritySinger.tsx
@@ -9,10 +9,31 @@ import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
 import SearchIcon from '@material-ui/icons/Search';
 import IconButton from '@material-ui/core/IconButton';
+import axios from 'axios';
 
 
 const SimilaritySinger = () => {
-
+    //多分これだと発火しない気がしてきた
+    //一番自然なのは親で取ってきてそれを子がもらうパターン
+    const sampleApiURL = 'https://jsondata.okiba.me/v1/json/6zYKL200924074420';
+    var data = 'ちょっとまって'
+    axios
+        .get(sampleApiURL)
+        .then((results) => {
+            // 以下のGoogle API のレスポンスの例を元に欲しいデータを取得
+            data = results.data;
+            // const result = data.results[0];
+            // const location = result.geometry.location;
+            // this.setState({
+            //     address: result.formatted_address,
+            //     lat: location.lat,
+            //     lng: location.lng,
+            // });
+        },
+        )
+        .catch(() => {
+            data = '通信に失敗しました。'
+        });
     const singers = ["鬼束", "aiko", "ZONE", "ZARD", "鬼束", "aiko", "ZONE", "ZARD"]
     const listItems = singers.map((singer: string) =>
         <Grid item xs={12}>
@@ -23,7 +44,7 @@ const SimilaritySinger = () => {
                         <Grid item xs={4}><Typography variant="h6">{singer}</Typography></Grid>
                         <Grid item xs={4}><Typography variant="h6">{singer}</Typography></Grid>
                         <Grid item xs={1}>
-                            <IconButton size="small" aria-label="add" style={{ backgroundColor: "#ffaacc" }}>
+                            <IconButton size="small" aria-label="add" style={{ backgroundColor: "#ffaacc" }} key={singer}>
                                 <AddIcon />
                             </IconButton>
                             {/* <Fab size="small" aria-label="add">
@@ -49,7 +70,8 @@ const SimilaritySinger = () => {
 
     return (
         <Grid container>
-            {listItems}
+            {/* {listItems} */}
+            <p>{data}</p>
         </Grid>
     );
 }
